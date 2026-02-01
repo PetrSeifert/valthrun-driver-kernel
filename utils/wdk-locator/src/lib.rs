a67065abd5eb82b101e82046f4297fc6b4bb01ea
sin <50749166+sinjs@users.noreply.github.com>
2026-02-01 19:39:19 +0100

    fix: improve wdk location logic in wdk-locator

--- utils/wdk-locator/src/lib.rs
+++ utils/wdk-locator/src/lib.rs
@@ -2,10 +2,14 @@ use std::path::{
     Path,
     PathBuf,
 };
+use std::env;
+use std::ffi::OsStr;
 
 use winreg::{
     enums::*,
     RegKey,
+    HKEY,
+    types::FromRegValue
 };
 
 #[derive(Debug)]
@@ -17,12 +21,74 @@ pub struct WindowsKit {
     pub dir_libs: PathBuf,
 }
 
-fn get_windows_kits_dir() -> anyhow::Result<PathBuf> {
-    let hklm = RegKey::predef(HKEY_LOCAL_MACHINE);
-    let key = r"SOFTWARE\Microsoft\Windows Kits\Installed Roots";
-    let dir: String = hklm.open_subkey(key)?.get_value("KitsRoot10")?;
+fn read_registry_value<T: FromRegValue>(
+    hkey: HKEY,
+    path: impl AsRef<OsStr>,
+    name: impl AsRef<OsStr>,
+) -> Option<T> {
+    let hkey = RegKey::predef(hkey);
+    let value = hkey.open_subkey(path).ok()?.get_value(name).ok()?;
 
-    Ok(dir.into())
+    Some(value)
+}
+
+fn get_windows_kits_dir() -> anyhow::Result<PathBuf> {
+    if let Ok(wdk_content_root) = env::var("WDKContentRoot") {
+        let path = Path::new(wdk_content_root.as_str());
+        if path.is_dir() {
+            return Ok(path.to_path_buf());
+        }
+        eprintln!(
+            "WDKContentRoot was detected to be {}, but does not exist or is not a valid directory.",
+            path.display()
+        );
+    }
+
+    if let Ok(microsoft_kit_root) = env::var("MicrosoftKitRoot") {
+        let path = Path::new(microsoft_kit_root.as_str());
+
+        if !path.is_absolute() {
+            eprintln!(
+                "MicrosoftKitRoot({}) was found in environment, but is not an absolute path.",
+                path.display()
+            );
+        } else if !path.is_dir() {
+            eprintln!(
+                "MicrosoftKitRoot({}) was found in environment, but does not exist or is not a \
+                 valid directory.",
+                path.display()
+            );
+        } else {
+            let wdk_kit_version = env::var("WDKKitVersion").unwrap_or_else(|_| "10.0".to_string());
+            let path = path.join("Windows Kits").join(wdk_kit_version);
+            if path.is_dir() {
+                return Ok(path);
+            }
+            eprintln!(
+                "WDKContentRoot was detected to be {}, but does not exist or is not a valid \
+                 directory.",
+                path.display()
+            );
+        }
+    }
+
+    if let Some(path) = read_registry_value::<String>(
+        HKEY_LOCAL_MACHINE,
+        r"SOFTWARE\Microsoft\Windows Kits\Installed Roots",
+        r"KitsRoot10",
+    ) {
+        return Ok(Path::new(path.as_str()).to_path_buf());
+    }
+
+    if let Some(path) = read_registry_value::<String>(
+        HKEY_LOCAL_MACHINE,
+        r"SOFTWARE\Wow6432Node\Microsoft\Windows Kits\Installed Roots",
+        r"KitsRoot10",
+    ) {
+        return Ok(Path::new(path.as_str()).to_path_buf());
+    }
+
+    anyhow::bail!("No valid Windows Kit found");
 }
 
 fn find_latest_version(windows_kits_dir: &PathBuf) -> anyhow::Result<String> {
